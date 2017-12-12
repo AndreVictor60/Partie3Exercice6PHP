c00b630415b61b23bf72f936d8c5427c249b826f
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:10:34 +0100

    fini

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 3 Exercice 6</title>
+  </head>
+  <body>
+    <?php
+      for($pas=20;$pas >= 0;$pas--){
+        echo 'C\'est presque bon.<br />';
+      }
+     ?>
+  </body>
+</html>
